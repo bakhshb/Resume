8a170b3c9ea6c73ec4b45136da3b33da8ade61c4
bakhshb <bakhshb@users.noreply.github.com>
2019-11-19 00:51:28 +0300

    bug fix
    bug fix for css

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from './Store';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@fortawesome/fontawesome-free/css/all.css';
-import '../src/assets/resume.css';
-import '@fortawesome/fontawesome-free/js/all.js';
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import 'jquery.easing/jquery.easing.min.js';
+import store from "./Store";
+import "bootstrap/dist/css/bootstrap.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "/src/assets/resume.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+import "jquery/dist/jquery.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "jquery.easing/jquery.easing.min.js";
 
 Vue.config.productionTip = false;
 

86621003c6d80b9fd547be41dab92044d1cff9b3
bakhshb <bakhshb@users.noreply.github.com>
2019-11-19 01:07:26 +0300

    Fix Css
    remove import local css and add css code to app.vue

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import store from "./Store";
 import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "/src/assets/resume.css";
+// import "./assets/resume.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "jquery/dist/jquery.min.js";
 import "bootstrap/dist/js/bootstrap.bundle.js";
